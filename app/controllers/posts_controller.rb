a70a230dda30aa5283e2fef5a9d2567519d3d02a
Yuri Nguyen <yuringuyen312@gmail.com>
2021-03-17 01:07:58 -0400

    commit tree 95de34fd6c68d77fbd1b1cb9c8dbcaed898f7c5d

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,18 +12,24 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(params["post"])
-	  @post.save
+		
+	  @post = Post.create(post_params(:title, :description))
 	  redirect_to post_path(@post)
 	end
 
 	def update
-	  @post = Post.find(params[:id])
-	  @post.update(params["post"])
+	 edit
+	  @post.update(post_params(:title))
 	  redirect_to post_path(@post)
 	end
 
 	def edit
 	  @post = Post.find(params[:id])
+	end
+
+	private
+
+	def post_params(*arg)
+		params.require(:post).permit(*arg)
 	end
 end
